985834363fcdc2bd6296c93bcb302d280bd2f667
Christian Vajgel <christian.vajgel@al.infnet.edu.br>
2019-11-01 07:55:37 -0300

    Aula_31OUT19

---
+++ calculator.js
@@ -0,0 +1,31 @@
+let number1,
+    number2,
+    result,
+    operation;
+
+// valueResult = document.querySelector("p#result");
+
+function calculate(event) {
+    number1 = Number(document.querySelector("input#number1").value);
+    number2 = Number(document.querySelector("input#number2").value);
+
+    operation = event.currentTarget.innerText;
+
+    switch (operation) {
+        case "+":
+            result = number1 + number2;alert(result); printResult(result); break;
+        case "-":
+            result = number1 - number2; printResult(result); break;
+        case "*":
+            result = number1 * number2; printResult(result); break;
+        case "/":
+            result = number1 / number2; printResult(result); break;
+        default:
+            result = 0;
+    }
+}
+
+function printResult(result) {
+    let valueResult = document.querySelector("p#result");
+    valueResult.innerText = result;
+}
